b8714edbb5d75a76835953d150f3c0577abe6099
Jonathan Aca <jonathanaca22@gmail.com>
2017-01-11 22:31:01 +0000

    I made tiny turtle move in motion of a house

---
+++ script.js
@@ -0,0 +1,15 @@
+    TinyTurtle.apply(window); 
+forward(50);
+right(90);
+//stamp()
+right(0)
+forward(100)
+right(90)
+forward(50)
+right(90)
+forward(100)
+right(25)
+forward(60)
+right(130)
+forward(60)
+
